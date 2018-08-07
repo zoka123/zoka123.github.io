1bda4824c204aa882e8a158122c13658772a4ce4
Zoran Antolovic <zoran.antolovic.os@gmail.com>
2018-08-07 19:14:50 +0200

    Temp website build

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+// See https://florianbrinkmann.com/en/4240/sass-webpack/
+
+const path = require('path');
+
+module.exports = {
+    entry: [
+        './src/index.js',
+        './src/index.scss'
+    ],
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'index.css',
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
